511d77baa977733ff1f82ac979a73c89cc989d76
Peixoto258 <js.peixotoalves@gmail.com>
2021-11-02 16:51:57 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into worker (name, daily_income) values ('Bob', 200.0);
+insert into worker (name, daily_income) values ('Maria', 300.0);
+insert into worker (name, daily_income) values ('Alex', 200.0);
