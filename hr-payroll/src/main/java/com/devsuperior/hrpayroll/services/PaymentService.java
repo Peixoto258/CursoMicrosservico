283d2fea3fcbb69af239ad0714531d93c2772e0f
Peixoto258 <js.peixotoalves@gmail.com>
2021-11-04 21:53:36 -0300

    Feign Client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,10 +2,9 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,18 +12,12 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}") // define o host nas propriedades
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate; // instancia o objeto para buscar o outro serviço
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerid, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerid); // mapeia os campos que eu vou buscar na url
 
-        // mapeia a url, classe que vou buscar e qual a variável
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerid).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
