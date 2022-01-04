26d18242ffff681d744bd93f79035e0a4fb5926f
Peixoto258 <js.peixotoalves@gmail.com>
2022-01-03 21:04:54 -0300

    UserRepository, UserResource, Zuul Config

---
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -0,0 +1,43 @@
+package com.devsuperior.hruser.resources;
+
+import java.util.List;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RefreshScope /* Colocar essa configuração em toda classe que acessa às configurações*/
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        User obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User obj = repository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+}
+
